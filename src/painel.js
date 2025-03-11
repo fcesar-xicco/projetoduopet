756ae295a44d230ebbf438000bc3ec1c4508b5e1
Francisco Cesar <fcesarbl@gmail.com>
2025-03-10 21:09:35 -0300

    atulização responsivel

--- src/painel.js
+++ src/painel.js
@@ -1,1 +1,25 @@
 // formulario - validações dos cadastro
+let areaCachorro = document.getElementById('area-cachorro')
+let abaPrincipal = document.querySelector('main.aba-principal')
+var body = document.getElementsByTagName('body')[0]
+// body.style.backgroundImage = "url('../imagens/ossinho2.jpg')"
+
+
+areaCachorro.addEventListener('click',() =>{
+    
+    mudarFundo(abaPrincipal)
+    
+})
+function mudarFundo(fundo){
+    const reader = new FileReader()
+    
+    reader.readAsDataURL(ossinho.jpg)
+    
+    reader.onload = function(evento){
+        const urlmage = evento.target.result
+
+        abaPrincipal.style.backgroundImage = `url('${urlmage}')`
+        alert()
+
+    }
+}
